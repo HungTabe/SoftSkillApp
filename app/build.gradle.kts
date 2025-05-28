d8556dc590c828d06a5343b9ac0b3e53a8c13e35
hungtabe <trandinhhung717@gmail.com>
2025-05-28 11:14:54 +0700

    [ssa_3] Splash Activity and Logo App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -40,4 +41,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+//    implementation("")
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation("androidx.media3:media3-exoplayer:1.4.0")
 }
