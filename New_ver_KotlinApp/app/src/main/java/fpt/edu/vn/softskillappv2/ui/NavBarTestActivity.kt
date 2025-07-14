939a0f632deaa70d010a94c7d37fde3c548aace2
hungtabe <trandinhhung717@gmail.com>
2025-07-14 17:35:26 +0700

    [ssa_15] Setup BottomNavBar

---
+++ New_ver_KotlinApp/app/src/main/java/fpt/edu/vn/softskillappv2/ui/NavBarTestActivity.kt
@@ -0,0 +1,69 @@
+package fpt.edu.vn.softskillappv2.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import fpt.edu.vn.softskillappv2.R
+import fpt.edu.vn.softskillappv2.databinding.ActivityMainBinding
+import fpt.edu.vn.softskillappv2.ui_fragment.create
+import fpt.edu.vn.softskillappv2.ui_fragment.discover
+import fpt.edu.vn.softskillappv2.ui_fragment.home
+import fpt.edu.vn.softskillappv2.ui_fragment.profile
+
+class NavBarTestActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_nav_bar_test)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        replaceFragment(home())
+        bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(home())
+                R.id.profile -> replaceFragment(profile())
+                R.id.discover -> replaceFragment(discover())
+                R.id.create -> replaceFragment(create())
+
+                else ->{
+
+
+
+                }
+
+            }
+
+            true
+
+        }
+
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
+    }
+
+}
